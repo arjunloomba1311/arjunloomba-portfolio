5f77664c4478dc3edf159ca2c484a281acf4c15a
arjunloomba1311 <arjunrajloomba@gmail.com>
2023-12-19 11:38:25 +0530

    Edited contact info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/ArjunRajLoomba",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@ArjunRajLoomba",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:arjunrajloomba@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "arjunrajloomba@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/arjunloomba1311",
 		label: "Github",
-		handle: "chronark",
+		handle: "@arjunloomba1311",
 	},
 ];
 
